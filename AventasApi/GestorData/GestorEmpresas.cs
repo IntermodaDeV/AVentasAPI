2a5f901019e49a864db572613e2265484d94c334
ChristianSanchez <christian.sanchez@cit.hn>
2020-03-30 08:56:13 -0600

    Agregando gestor empresas

---
+++ AventasApi/GestorData/GestorEmpresas.cs
@@ -0,0 +1,59 @@
+﻿using AventasApi.Enviroments;
+using AventasApi.Infrastructure;
+using AventasApi.Models.ApiModels;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AventasApi.GestorData
+{
+    public class GestorEmpresas
+    {
+        private static readonly string UrlString = $"{Enviroment.CRMWebServiceURLApi}empresa/empresas";
+        public static bool empresasAgregadas = false;
+        public static bool errorAlAgregar = false;
+
+        public static async Task ObtenerEmpresas()
+        {
+            try
+            {
+
+                    var restClient = new RestClient(UrlString);
+                    var request = new RestRequest(Method.GET);
+                    request.AddHeader("Accept", "application/json");
+                    IRestResponse response = restClient.Execute(request);
+
+                    if (response.IsSuccessful)
+                    {
+                        List<EmpresasCRMApiModel> empresas = JsonConvert.DeserializeObject<List<EmpresasCRMApiModel>>(response.Content);
+
+                        var validarEmpresaEsValida = empresas != null && empresas.Count > 0;
+                        if (validarEmpresaEsValida)
+                        {
+
+                            foreach (var empresa in empresas)
+                            {
+                                validarEmpresaEsValida = empresa != null;
+                                if (validarEmpresaEsValida)
+                                {
+                                    
+                                }
+
+                            }
+                        }
+                    }
+      
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
---
+++ AventasApi/Models/ApiModels/EmpresasCRMApiModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AventasApi.Models.ApiModels
+{
+    public class EmpresasCRMApiModel
+    {
+        public int id { get; set; }
+
+        public string COMPANY_CODE { get; set; }
+
+        public string NAME { get; set; }
+
+        public string ADDRESS { get; set; }
+
+        public string NIFCIF { get; set; }
+    }
+}
