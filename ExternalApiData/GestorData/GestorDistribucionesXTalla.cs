dbd501d623e600a4f6154607b3aae2e096164388
Aaron Lemus <aaron.lemus@cit.hn>
2020-04-10 12:19:27 -0600

    update hestores

--- ExternalApiData/GestorData/GestorDistribucionesXTalla.cs
+++ ExternalApiData/GestorData/GestorDistribucionesXTalla.cs
@@ -1,101 +1,33 @@
-﻿using ExternalApiData.Enviroments;
-using DBData.Database;
-using ExternalApiData.Models.ApiModels;
-using ExternalApiData.Models.ViewModels;
+﻿using DBData.Database;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
+using ExternalApiData.Models.ApiModels;
+using ExternalApiData.Enviroments;
+using ExternalApiData.Models;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace ExternalApiData.GestorData
 {
     public class GestorDistribucionesXTalla
     {
         private static string UrlString = $"{Enviroment.CRMWebServiceURLApi}productos/imhn/distribucion";
-        private static HttpClient client = new ClienteHttp();
-        public static Task TaskActualizarLineas;
-
-        //private static AVentasEntities context = new AVentasEntities();
-
-
-        static GestorDistribucionesXTalla()
+        public async Task<List<DistribucionXTallaApiModel>> Obtener()
         {
-            ReiniciarTaskActualizarLineas();
-
-        }
-        public static async void ReiniciarTaskActualizarLineas()
-        {
-
-            //List<TallasXGrupo> tallasXGrupos = new List<TallasXGrupo>();
-            //using (AVentasEntities context = new AVentasEntities())
-            //{
-            //    tallasXGrupos = context.TallasXGrupo.ToList();
-            //}
-            TaskActualizarLineas = new Task(async () =>
+            string peticion = string.Format(UrlString);
+            var restClient = new RestClient(peticion)
             {
-
-                //List<Clientes> clientes = new List<Clientes>();
-                var distribuciones = new List<DistribucionXTallaApiModel>();
-                HttpResponseMessage response = await client.GetAsync(UrlString).ConfigureAwait(false);
-                if (response.IsSuccessStatusCode)
-                {
-                    distribuciones = await response.Content.ReadAsAsync<List<DistribucionXTallaApiModel>>();
-                }
-                if (distribuciones != null && distribuciones.Count > 0)
-                {
-                    var taskGetacuerdos =
-                    distribuciones.Select(async col =>
-                    {
-                        using (AVentasEntities context = new AVentasEntities())
-                        {
-                            try
-                            {
-                                if (col.DIST_QTY != "0.00" && col.DIST_QTY != ".00")
-                                {
-                                    //Debug.WriteLine(col.DIST_QTY);
-                                    var tallaEmpaque = context.TallasXGrupo.FirstOrDefault(tallXGrup => col.SIZE_CHART == tallXGrup.GrupoTalla.CodigoGrupoTalla && tallXGrup.CodigoTalla == col.DISTRIBUTION);
-                                    if (tallaEmpaque != null)
-                                    {
-                                    //var distribucionDB = context.DistribucionxTalla.FirstOrDefault(distr => tallaEmpaque.IdTallaxGrupo == distr.IdTallaxGrupo && distr.NombreTalla == col.DIST_SIZE);
-                                    if (false)
-                                    {
-                                        //distribucionDB.NombreDistribucion = distribucionDB.NombreDistribucion;
-                                        //distribucionDB.Cantidad = distribucionDB.Cantidad;
-                                    }
-                                    else
-                                    {
-
-                                        DistribucionxTalla distribucion = new DistribucionxTalla
-                                        {
-                                            IdTallaxGrupo = tallaEmpaque.IdTallaxGrupo,
-                                            NombreDistribucion = col.DIST_NAME,
-                                            NombreTalla = col.DIST_SIZE,
-                                            Cantidad = col.DIST_QTY,
-                                        };
-                                        context.DistribucionxTalla.Add(distribucion);
-                                    }
-                                    context.SaveChanges();
-                                }
-                                }
-                                else
-                                {
-                                }
-                            }
-                            catch (Exception e)
-                            {
-                                Debug.WriteLine(e);
-                            }
-                        }
-                    });
-                    await Task.WhenAll(taskGetacuerdos);
-
-                    Debug.WriteLine("FFinalizo");
-                }
-            });
+                Timeout = 600 * 1000
+            };
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Accept", "application/json");
+            IRestResponse response = restClient.Execute(request);
+            var listaDeserializada = JsonConvert.DeserializeObject<List<DistribucionXTallaApiModel>>(response.Content);
+            return listaDeserializada;
         }
     }
 }
--- ExternalApiData/GestorData/GestorTipoPaquete.cs
+++ ExternalApiData/GestorData/GestorTipoPaquete.cs
@@ -1,71 +1,33 @@
-﻿using System;
+﻿using DBData.Database;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
-using ExternalApiData.Enviroments;
-using DBData.Database;
 using ExternalApiData.Models.ApiModels;
+using ExternalApiData.Enviroments;
+using ExternalApiData.Models;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace ExternalApiData.GestorData
 {
-public class GestorTipoPaquete
+    public class GestorTipoPaquete
     {
         private static string UrlString = $"{Enviroment.CRMWebServiceURLApi}api/paquetes/imhn";
-        private static HttpClient client = new ClienteHttp();
-        public static Task TaskActualizarLineas;
-
-        //private static AVentasEntities context = new AVentasEntities();
-
-
-        static GestorTipoPaquete()
+        public async Task<List<TiposdeColeccion>> Obtener()
         {
-            ReiniciarTaskActualizarLineas();
-
-        }
-        public static async void ReiniciarTaskActualizarLineas()
-        {
-
-
-            TaskActualizarLineas = new Task(async () =>
+            string peticion = string.Format(UrlString);
+            var restClient = new RestClient(peticion)
             {
-
-
-                List<ColeccionesCRMApiViewModel> colecciones = new List<ColeccionesCRMApiViewModel>();
-                HttpResponseMessage response = await client.GetAsync(UrlString).ConfigureAwait(false);
-                if (response.IsSuccessStatusCode)
-                {
-                    colecciones = await response.Content.ReadAsAsync<List<ColeccionesCRMApiViewModel>>();
-                    List<TiposdeColeccion> tipoColecciones = new List<TiposdeColeccion>();
-                    foreach (var coleccion in colecciones)
-                    {
-                        if (!tipoColecciones.Exists(tipCol => tipCol.ColeccionTipo == coleccion.PACKAGE_TYPE))
-                        {
-                            tipoColecciones.Add(new TiposdeColeccion
-                            {
-                                ColeccionTipo = coleccion.PACKAGE_TYPE,
-                                Descripcion = coleccion.PACKAGE_TYPE_NAME
-                            });
-                        }
-                        
-                    }
-                    using (AVentasEntities context = new AVentasEntities())
-                    {
-                        context.TiposdeColeccion.AddRange(tipoColecciones);
-                        context.SaveChanges();
-                    }
-                    
-                    Debug.WriteLine("FFinalizo");
-                }
-                else
-                {
-                    Debug.WriteLine("Error en a peticion");
-
-                }
-            });
+                Timeout = 600 * 1000
+            };
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Accept", "application/json");
+            IRestResponse response = restClient.Execute(request);
+            var listaDeserializada = JsonConvert.DeserializeObject<List<TiposdeColeccion>>(response.Content);
+            return listaDeserializada;
         }
     }
 }
