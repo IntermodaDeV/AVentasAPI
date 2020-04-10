4f50abd958bff9eee4a7c9736b5845cc7cf68b51
ChristianSanchez <christian.sanchez@cit.hn>
2020-04-10 08:34:12 -0600

    GestorTipoPaquete

---
+++ DataManager/Extensions/TipoPaqueteExtension.cs
@@ -0,0 +1,18 @@
+﻿using DBData.Database;
+using ExternalApiData.Models.ApiModels;
+
+namespace DataManager.Extensions
+{
+    public static class TipoPaqueteExtension
+    {
+        public static TiposdeColeccion CreandoTipoColeccion(this ColeccionesCRMApiViewModel coleccion)
+        {
+            TiposdeColeccion nuevoTipoColeccion = new TiposdeColeccion()
+            {
+                ColeccionTipo = coleccion.PACKAGE_TYPE,
+                Descripcion = coleccion.PACKAGE_TYPE_NAME
+            };
+            return nuevoTipoColeccion;
+        }
+    }
+}
--- ExternalApiData/GestorData/GestorTipoPaquete.cs
+++ ExternalApiData/GestorData/GestorTipoPaquete.cs
@@ -1,71 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Web;
 using ExternalApiData.Enviroments;
-using DBData.Database;
 using ExternalApiData.Models.ApiModels;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace ExternalApiData.GestorData
 {
-public class GestorTipoPaquete
+    public class GestorTipoPaquete
     {
-        private static string UrlString = $"{Enviroment.CRMWebServiceURLApi}api/paquetes/imhn";
-        private static HttpClient client = new ClienteHttp();
-        public static Task TaskActualizarLineas;
+        private static readonly string UrlString = $"{Enviroment.CRMWebServiceURLApi}paquetes/imhn";
 
-        //private static AVentasEntities context = new AVentasEntities();
-
-
-        static GestorTipoPaquete()
+        public async Task<List<ColeccionesCRMApiViewModel>> ObtenerTiposDesdeCRMAPI()
         {
-            ReiniciarTaskActualizarLineas();
-
-        }
-        public static async void ReiniciarTaskActualizarLineas()
-        {
-
-
-            TaskActualizarLineas = new Task(async () =>
+            var colecciones = new List<ColeccionesCRMApiViewModel>();
+            await Task.Run(() =>
             {
+                var restClient = new RestClient(UrlString);
+                var request = new RestRequest(Method.GET);
+                request.AddHeader("Accept", "application/json");
+                IRestResponse response = restClient.Execute(request);
 
-
-                List<ColeccionesCRMApiViewModel> colecciones = new List<ColeccionesCRMApiViewModel>();
-                HttpResponseMessage response = await client.GetAsync(UrlString).ConfigureAwait(false);
-                if (response.IsSuccessStatusCode)
+                if (response.IsSuccessful)
                 {
-                    colecciones = await response.Content.ReadAsAsync<List<ColeccionesCRMApiViewModel>>();
-                    List<TiposdeColeccion> tipoColecciones = new List<TiposdeColeccion>();
-                    foreach (var coleccion in colecciones)
-                    {
-                        if (!tipoColecciones.Exists(tipCol => tipCol.ColeccionTipo == coleccion.PACKAGE_TYPE))
-                        {
-                            tipoColecciones.Add(new TiposdeColeccion
-                            {
-                                ColeccionTipo = coleccion.PACKAGE_TYPE,
-                                Descripcion = coleccion.PACKAGE_TYPE_NAME
-                            });
-                        }
-                        
-                    }
-                    using (AVentasEntities context = new AVentasEntities())
-                    {
-                        context.TiposdeColeccion.AddRange(tipoColecciones);
-                        context.SaveChanges();
-                    }
-                    
-                    Debug.WriteLine("FFinalizo");
-                }
-                else
-                {
-                    Debug.WriteLine("Error en a peticion");
-
+                    colecciones = JsonConvert.DeserializeObject<List<ColeccionesCRMApiViewModel>>(response.Content);
                 }
             });
+            return colecciones;
         }
     }
 }
