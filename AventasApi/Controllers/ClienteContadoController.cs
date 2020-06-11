071da979b8da3afbb07b2b763e66016a241e22a5
Francisco A. Garcia <frangarcia38@hotmail.com>
2020-06-11 10:22:20 -0600

    Commit con los siguientes cambios:
    - Se añade modelo y controlador para cliente contado.
    - Se añade validacion por parametrizacion si se enviaran los telefonos para clientes de contado y credito.
    - Se añade porcentaje de impuesto por pais.
    - Se añaden los campos necesarios para el funcionamiento necesario para los pedidos de cliente contado.

---
+++ AventasApi/Controllers/ClienteContadoController.cs
@@ -0,0 +1,77 @@
+﻿using DBData.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AventasApi.Controllers
+{
+    [RoutePrefix("api/clientecontado")]
+    public class ClienteContadoController : ApiController
+    {
+        AVentasEntities context = new AVentasEntities();
+        [HttpPost]
+        public IHttpActionResult CreateClienteContado([FromBody] ClienteContado cliente)
+        {
+            try
+            {
+                if (cliente == null)
+                {
+                    return BadRequest();
+                }
+                context.ClienteContado.Add(cliente);
+                context.SaveChanges();
+                return Ok(cliente);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+
+        }
+        [HttpPost]
+        [Route("edit")]
+        public IHttpActionResult EditClienteContado([FromBody] ClienteContado cliente)
+        {
+            try
+            {
+                var item = context.ClienteContado.Find(cliente.id);
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                item.Nombre = cliente.Nombre;
+                item.RTN = cliente.RTN;
+                item.Telefono = cliente.Telefono;
+                item.FlagClientePotencial = cliente.FlagClientePotencial;
+                item.Direccion = cliente.Direccion;
+                context.SaveChanges();
+                return Ok(item);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet]
+        [Route("{ruta}")]
+        public IHttpActionResult ClienteContadoList(string ruta)
+        {
+            try
+            {
+                var clientes = context.ClienteContado.Where(x => x.Ruta == ruta).OrderBy(x => x.Nombre).ToList();
+                if (clientes.Count <= 0)
+                {
+                    return NotFound();
+                }
+                return Ok(clientes);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
