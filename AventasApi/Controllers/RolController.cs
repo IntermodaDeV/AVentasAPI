d68a4f32df9856cd4d566b73dfa2989e258e8ed4
Francisco A. Garcia <frangarcia38@hotmail.com>
2020-10-21 15:18:53 -0600

    Commit con los siguientes cambios: - Se crea el controlador de roles con funcionalidades CRUD. - Se crean modelos para el controladore de roles.

---
+++ AventasApi/Controllers/RolController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AventasApi.Models;
+using DBData.Database;
+
+namespace AventasApi.Controllers
+{
+    public class RolController : ApiController
+    {
+        [HttpGet]
+        [Route("~/api/rol/roles")]
+        public async Task<IHttpActionResult> ObtenerRoles()
+        {
+            try
+            {
+                using(var ctx = new AVentasEntities())
+                {
+                    var listaRoles = await ctx.Roles.Select(x=>new { Id=x.Id,Nombre=x.Nombre,Status=x.Status }).ToListAsync();
+                    return Ok(listaRoles);
+                }
+            }catch(Exception e)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("~/api/rol/estado/{Id}")]
+        public async Task<IHttpActionResult> ModificarEstado(int Id)
+        {
+            try
+            {
+                using (var ctx = new AVentasEntities())
+                {
+                    var rol = await ctx.Roles.FindAsync(Id);
+
+                    if (rol == null)
+                    {
+                        return BadRequest("No se encuentra el rol.");
+                    }
+
+                    rol.Status = !rol.Status;
+                    var result = await ctx.SaveChangesAsync();
+                    return Ok(result);
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("~/api/rol/crear")]
+        public async Task<IHttpActionResult> CrearRol([FromBody] RolCrearModel rol)
+        {
+            try
+            {
+                using (var ctx = new AVentasEntities())
+                {
+                    var nuevoRol = new Roles() { Status = rol.Status, Nombre = rol.Nombre };
+                    ctx.Roles.Add(nuevoRol);
+                    var result = await ctx.SaveChangesAsync();
+                    return Ok(result);
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("~/api/rol/modificar")]
+        public async Task<IHttpActionResult> ModificarRol([FromBody] RolModificarModel rol)
+        {
+            try
+            {
+                using (var ctx = new AVentasEntities())
+                {
+                    var rolBd = await ctx.Roles.FindAsync(rol.Id);
+
+                    if(rolBd == null)
+                    {
+                        return BadRequest("No se encuentra el rol");
+                    }
+
+                    rolBd.Nombre = rol.Nombre;
+                    rolBd.Status = rol.Status;
+                    var result = await ctx.SaveChangesAsync();
+                    return Ok(result);
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ AventasApi/Models/RolCrearModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AventasApi.Models
+{
+    public class RolCrearModel
+    {
+        public string Nombre { get; set; }
+        public bool Status { get; set; }
+    }
+}
