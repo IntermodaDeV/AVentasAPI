7f6ef16d6746d877faad50ea7bdc7755a9101c0c
Francisco A. Garcia <frangarcia38@hotmail.com>
2020-11-03 09:26:04 -0600

    Commit con los siguientes cambios: - Se captura la excepcion proveniente de AX al momento de generar un pedido.

---
+++ ExternalApiData/ApiModels/ApiException.cs
@@ -0,0 +1,9 @@
+﻿namespace ExternalApiData.ApiModels
+{
+    public class ApiException
+    {
+        public string Message { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string ExceptionType { get; set; }
+    }
+}
