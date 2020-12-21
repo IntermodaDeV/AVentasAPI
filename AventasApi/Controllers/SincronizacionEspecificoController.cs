439a289583ca75dcbaf4810e417dc97ca6dd8f97
Francisco A. Garcia <frangarcia38@hotmail.com>
2020-12-21 14:02:22 -0600

    Commit con los siguientes cambios: - Actualizacion dbset relacion lista ejecucion especifica y gestor. - Endpoint que retorna lista de ejecucion especifica.

--- AventasApi/Controllers/SincronizacionEspecificoController.cs
+++ AventasApi/Controllers/SincronizacionEspecificoController.cs
@@ -2,6 +2,7 @@
 using DBData.Database;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -66,6 +67,48 @@ namespace AventasApi.Controllers
             catch (System.Exception ex)
             {
                 return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{usuario}")]
+        public IHttpActionResult GetListado(string usuario)
+        {
+            try
+            {
+                //var fecha = DateTime.ParseExact(date, "yyyyMMddHHmmss", null);
+                var fechaInicio = DateTime.Now.Date.AddDays(-15);
+
+                using (var context = new AVentasConfigEntities())
+                {
+                    var modulos = context.LISTA_EJECUCION_ESPECIFICO.Where(x => x.USUARIO == usuario && x.FECHA >= fechaInicio).ToList();
+                    if (modulos.Count <= 0)
+                    {
+                        return NotFound();
+                    }
+
+                    var modulosList = modulos.Select(x => new ListaEjecucionManuaVisuallModel()
+                    {
+                        ID = x.ID,
+                        ID_GESTOR = x.ID_GESTOR,
+                        NOMBRE = x.GESTORES.ETIQUETA,
+                        FECHASTR = x.FECHA.HasValue ? x.FECHA.Value.ToString("dd/MM/yyyy hh:mm tt") : "",
+                        FECHA = x.FECHA,
+                        USUARIO = x.USUARIO,
+                        EN_ESPERA = x.EN_ESPERA,
+                        EN_EJECUCION = x.EN_EJECUCION,
+                        FINALIZADO = x.FINALIZADO,
+                        ID_MODULO = x.GESTORES.MODULOS.ID,
+                        MODULO = x.GESTORES.MODULOS.NOMBRE
+                    }
+                    ).ToList();
+
+                    return Ok(modulosList);
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
             }
         }
     }
