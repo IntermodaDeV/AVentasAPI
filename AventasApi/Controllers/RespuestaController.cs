32ab082380cd172ab2c7444aab1568951c04650a
Keyla Yanori Soriano Herrera <keylasoriano45@hotmail.com>
2021-02-12 14:17:11 -0600

    Commit con los siguientes cambios: - Se modifica metodo de posteo de respuestas y respuestas detalle. - Se añade el campo preguntasOpciones en respuestasDetalleViewModel - Se añade metodo de obtencion de encuestasActivas en encuestascontroller - Se añade metodo de SeccionesEncuestasSelected en encuesta controller

--- AventasApi/Controllers/RespuestaController.cs
+++ AventasApi/Controllers/RespuestaController.cs
@@ -22,17 +22,22 @@ namespace AventasApi.Controllers
             {
                 using (var db = new AVentasEntities())
                 {
+                    if(Respuestas.RespuestasDetalle.Count() > 0)
+                    {
                     List<RespuestasViewModel> RespuestasEncuestas = new List<RespuestasViewModel>();
                     RespuestasEncuestas.Add(Respuestas);
 
-                    var RespuestaDB = await db.Respuestas.FirstOrDefaultAsync(r => r.CodigoCliente == Respuestas.CodigoCliente && r.EncuestaId == Respuestas.EncuestaId && r.UsuarioId == Respuestas.UsuarioId);
+                    var usuario = db.Usuarios.FirstOrDefault(u => u.usuario == Respuestas.Usuario);
+                    var RespuestaDB = await db.Respuestas.FirstOrDefaultAsync(r => r.CodigoCliente == Respuestas.CodigoCliente && r.EncuestaId == Respuestas.EncuestaId && r.UsuarioId == usuario.Id);
+
+
                     var respuestaId = 0;
                     if(RespuestaDB == null)
                     {
                         var Respuesta = new Respuestas()
                         {
                             CodigoCliente = Respuestas.CodigoCliente,
-                            UsuarioId = Respuestas.UsuarioId,
+                            UsuarioId = usuario.Id,
                             EncuestaId = Respuestas.EncuestaId,
                             CreatedBy = Respuestas.Usuario,
                             CreatedDate = DateTime.Now
@@ -48,9 +53,11 @@ namespace AventasApi.Controllers
                         respuestaId = RespuestaDB.Id;
                     }
 
-                    _ = RegistrarRespuestasDetalle(Respuestas.RespuestasDetalle, respuestaId);
+                    _ = RegistrarRespuestasDetalle(Respuestas.RespuestasDetalle, respuestaId, Respuestas.Usuario);
 
                     return Ok("Ok");
+                    }
+                    return BadRequest("La encuesta esta vacia");
                 }
             }
             catch (Exception e)
@@ -61,36 +68,48 @@ namespace AventasApi.Controllers
 
         [HttpPost]
         [Route("~/api/respuestasdetalle/registrar")]
-        public async Task<IHttpActionResult> RegistrarRespuestasDetalle(List<RespuestasDetalleViewModel> RespuestasDetalle, int respuestaId)
+        public async Task<IHttpActionResult> RegistrarRespuestasDetalle(List<RespuestasDetalleViewModel> RespuestasDetalle, int respuestaId, string Usuario)
         {
-            try
-            {
                 using (var db = new AVentasEntities())
                 {
+                   
                     foreach(var detalle in RespuestasDetalle)
                     {
-                        var RespuestaDetalle = new RespuestaDetalle()
+                        if(detalle.PreguntasOpciones != null && detalle.PreguntasOpciones.Count() > 0)
                         {
-                            RespuestaId = respuestaId,
-                            PreguntaId = detalle.PreguntaId,
-                            PreguntaOpcionesId = detalle.PreguntaOpcionesId,
-                            RespuestaAlfanumerica = detalle.RespuestaAlfanumerica,
-                            RespuestaNumerica = detalle.RespuestaNumerica,
-                            CreatedBy = detalle.Usuario,
-                            CreatedDate = DateTime.Now
-                        };
-                        db.RespuestaDetalle.Add(RespuestaDetalle);
+                            foreach (var det in detalle.PreguntasOpciones)
+                            {
+                                var RespuestaDetalle = new RespuestaDetalle()
+                                {
+                                    RespuestaId = respuestaId,
+                                    PreguntaId = detalle.PreguntaId,
+                                    PreguntaOpcionesId = Convert.ToInt32(det),
+                                    RespuestaAlfanumerica = detalle.RespuestaAlfanumerica,
+                                    RespuestaNumerica = detalle.RespuestaNumerica,
+                                    CreatedBy = Usuario,
+                                    CreatedDate = DateTime.Now
+                                };
+                                db.RespuestaDetalle.Add(RespuestaDetalle);
+                            }
+                        }
+                        else
+                        {
+                            var RespuestaDetalle = new RespuestaDetalle()
+                            {
+                                RespuestaId = respuestaId,
+                                PreguntaId = detalle.PreguntaId,
+                                PreguntaOpcionesId = detalle.PreguntasOpcionesId,
+                                RespuestaAlfanumerica = detalle.RespuestaAlfanumerica,
+                                RespuestaNumerica = detalle.RespuestaNumerica,
+                                CreatedBy = Usuario,
+                                CreatedDate = DateTime.Now
+                            };
+                            db.RespuestaDetalle.Add(RespuestaDetalle);
+                        }
                     }
-                   
-                   
                     var result = await db.SaveChangesAsync();
                     return Ok(result);
                 }
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.ToString());
-            }
         }
     }
 }
--- AventasApi/Models/ViewModels/RespuestasDetalleViewModel.cs
+++ AventasApi/Models/ViewModels/RespuestasDetalleViewModel.cs
@@ -10,7 +10,8 @@ namespace AventasApi.Models.ViewModels
         public int Id { get; set; }
         public int RespuestaId { get; set; }
         public int PreguntaId { get; set; }
-        public int? PreguntaOpcionesId { get; set; }
+        public int? PreguntasOpcionesId { get; set; }
+        public List<string> PreguntasOpciones { get; set; }
         public string RespuestaAlfanumerica { get; set; }
         public int RespuestaNumerica { get; set; }
         public string Usuario { get; set; }
