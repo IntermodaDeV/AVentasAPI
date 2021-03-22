8576377e6413affc4e22cc2e06d44912e92c226b
Francisco A. Garcia <frangarcia38@hotmail.com>
2021-03-22 11:18:24 -0600

    Creacion de enpoint en productosxcoleccion para obtener los productos en base al sitio y almacen.

--- AventasApi/Controllers/ColeccionesXLineaController.cs
+++ AventasApi/Controllers/ColeccionesXLineaController.cs
@@ -432,6 +432,136 @@ namespace AventasApi.Controllers
         }
 
         [HttpGet]
+        [Route("~/api/colecciones/productos/{coleccion}/{grupoprecio}/{pais}/{sitio}/{almacen}")]
+        public async Task<IHttpActionResult> GetProductosPorColeccionBodega(string coleccion, string grupoprecio, string pais,string sitio,string almacen)
+        {
+            try
+            {
+                using (var ctx = new AVentasEntities())
+                {
+                    string urlImagenes = ctx.Configuraciones.FirstOrDefault(conf => conf.CodigoConfiguracion == "UrlImages")?.Valor ?? "";
+                    bool filtarXGrupoPrecio = grupoprecio != null;
+                    List<ColeccionViewModel> colecciones = await ctx.Colecciones
+                        .Where(vw_coleccion => vw_coleccion.EmpresaId.ToUpper() == pais.ToUpper() && vw_coleccion.CodigoColeccion == coleccion.ToUpper())
+                        .Select(vw_coleccion =>
+                             new ColeccionViewModel
+                             {
+                                 Edades = vw_coleccion.EdadesxColeccion
+                               .OrderBy(me => me.MaestroEdad.Orden).Select(me => new EdadesViewModel
+                               {
+                                   IdEdad = me.IdEdad,
+                                   Edad = me.MaestroEdad.Edad,
+                                   Orden = me.MaestroEdad.Orden,
+                                   ProductosXEdad = ctx.ProductosxColeccion.Where(pxc => pxc.EmpresaId == pais.ToUpper() && pxc.IdColeccion == vw_coleccion.IdColeccion && pxc.IdEdad == me.IdEdad && pxc.IdLinea == me.IdLinea && pxc.VisibleParaVentas == true).Select(pxc => new ProductoXColeccionViewModel
+                                   {
+                                       ProductoId = pxc.CodigoProducto,
+                                       CodigoColeccion = vw_coleccion.CodigoColeccion,
+                                       CantidadMinima = pxc.CantidadMinima == null ? 0 : pxc.CantidadMinima,
+                                       CodigoProducto = pxc.IdProducto,
+                                       NombreProducto = pxc.NombreProducto,
+                                       GrupoImpuesto = (string.IsNullOrEmpty(pxc.GrupoImpuesto)) ? "GENERAL" : pxc.GrupoImpuesto.ToUpper(),
+                                       Precio = pxc.PreciosxProducto.Where(preEsp =>/* true || */preEsp.GrupoPrecio == grupoprecio).Select(precio => new PrecioXProductoViewModel
+                                       {
+                                           GrupoPrecio = precio.GrupoPrecio,
+                                           IdMoneda = precio.IdMoneda,
+                                           Precio = precio.Hasta == new DateTime(1900, 1, 1) ? precio.Precio : 0
+                                       }).ToList(),
+                                       GrupoTalla = pxc.CodigoGrupoTalla,
+                                       Linea = new LineaViewModel
+                                       {
+                                           IdLinea = pxc.MaestroLinea.IdLinea,
+                                           Linea = pxc.MaestroLinea.Linea,
+                                       },
+                                       AtributosXProducto = pxc.AtributosxProducto.Select(atr => new AtributosViewModel
+                                       {
+                                           Descripcion = (atr.Descripcion2 == "BASE") ? atr.CodigoAtributo + " - " + atr.Descripcion1 : atr.Descripcion1,
+                                           Tipo = atr.Descripcion2,
+                                           IdLinea = pxc.IdLinea
+                                       }).ToList(),
+                                       ListaImagenes = pxc.FotografiasXProducto.Where(txp => txp.FotografiaProducto != null)
+                                              .Where(txp => (pxc.FotografiasXProducto.Where(fxp => fxp.CodigoColor == "").Count() > 0 && txp.CodigoColor == "") || (pxc.FotografiasXProducto.FirstOrDefault() != null && txp.CodigoColor == pxc.FotografiasXProducto.FirstOrDefault().CodigoColor))
+                                              .OrderByDescending(foto => foto.Principal)
+                                              .Select(foto => new FotografiasXProductoViewModel
+                                              {
+                                                  IdFotografia = foto.IdFotografia,
+                                                  FotografiaProducto = urlImagenes + foto.FotografiaProducto,
+                                                  NombreFotografia = foto.FotografiaProducto,
+                                                  CodigoColor = foto.CodigoColor,
+                                                  Principal = foto.Principal ?? false
+                                              }).ToList(),
+                                       ListaTalla = ctx.TallasxProducto.Where(txp => txp.IdProducto == pxc.IdProducto && txp.IdTallaxGrupo != null).Select(txp => txp.TallasXGrupo).Where(txp => false || (vw_coleccion.ColeccionTipo == "F") || txp.DistribucionxTalla.Count > 0 || pxc.FisicoDisponible.Where(f => f.CodigoTalla == txp.CodigoTalla).Sum(f => f.Disponible) >= 0)
+                                           .Select(txp => new TallaViewModel
+                                           {
+                                               Talla = txp.CodigoTalla,
+                                               GrupoTallaId = txp.CodigoGrupoTalla,
+                                               Orden = txp.Orden ?? 0,
+                                               Distribucion = txp.DistribucionxTalla.Where(dis => dis.IdTallaxGrupo == txp.IdTallaxGrupo && dis.Cantidad != ".00").Select(dis => new DistribucionXTallaViewModel
+                                               {
+                                                   IdDistribucion = dis.IdDistribucion,
+                                                   IdTallaxGrupo = dis.IdTallaxGrupo,
+                                                   NombreDistribucion = dis.NombreDistribucion,
+                                                   NombreTalla = dis.NombreTalla,
+                                                   Cantidad = dis.Cantidad,
+                                                   Orden = dis.Orden,
+                                               }).OrderBy(or => or.Orden).ToList(),
+
+                                           }).OrderBy(txp => txp.Orden).ToList(),
+                                       ListaColores = pxc.ColoresxProducto.Where(cpp => (vw_coleccion.ColeccionTipo == "F") || cpp.Colores.FisicoDisponible.Where(f => f.IdProducto == pxc.IdProducto).Sum(f => f.Disponible) > 0).OrderBy(cpp => cpp.Colores.Color).Select(cpp => new ColorViewModel
+                                       {
+                                           CodigoColor = cpp.Colores.CodigoColor,
+                                           NombreColor = cpp.Colores.Color,
+                                           Color = cpp.Colores.Rgb,
+                                           ListaImagenes = pxc.FotografiasXProducto.Where(txp => txp.FotografiaProducto != null && txp.CodigoColor == cpp.Colores.CodigoColor).Select(foto => new FotografiasXProductoViewModel
+                                           {
+                                               IdFotografia = foto.IdFotografia,
+                                               FotografiaProducto = urlImagenes + foto.FotografiaProducto,
+                                               NombreFotografia = foto.FotografiaProducto,
+                                               CodigoColor = foto.CodigoColor,
+                                               Principal = foto.Principal ?? false
+                                           }).ToList(),
+
+                                       }).ToList(),
+                                       ListaColoresSinStock = pxc.ColoresxProducto.Where(cpp => (vw_coleccion.ColeccionTipo == "F") || cpp.Colores.FisicoDisponible.Where(f => f.IdProducto == pxc.IdProducto).Sum(f => f.Disponible) == 0).OrderBy(cpp => cpp.Colores.Color).Select(cpp => new ColorSinStock
+                                       {
+                                           CodigoColor = cpp.Colores.CodigoColor,
+                                           NombreColor = cpp.Colores.Color,
+                                           Color = cpp.Colores.Rgb,
+                                       }).ToList(),
+                                       fisicaDisponible = pxc.FisicoDisponible
+                                              .Where(f => f.Sitio==sitio && f.Almacen==almacen)
+                                              .Select(f => new FisicoDisponibleViewModel
+                                              {
+                                                  CodigoColor = f.CodigoColor,
+                                                  IdTalla = f.CodigoTalla,
+                                                  Cantidad = f.Disponible < 0 ? 0 : f.Disponible,
+                                                  MinStock = f.MinStock,
+                                                  PreciosEspecificos = f.PrecioEspecifico.Where(preEsp => filtarXGrupoPrecio).Where(preEsp => preEsp.GrupoPrecio == grupoprecio).Select(preEsp => new PrecioEspecificoViewModel
+                                                  {
+                                                      IdPrecioEspecifico = preEsp.IdPrecioEspecifico,
+                                                      IdMoneda = preEsp.IdMoneda,
+                                                      IdProducto = preEsp.IdProducto,
+                                                      GrupoPrecio = preEsp.GrupoPrecio,
+                                                      IdFisicoDisponible = preEsp.IdFisicoDisponible,
+                                                      Precio = preEsp.Hasta == new DateTime(1900, 1, 1) ? preEsp.Precio : pxc.PreciosxProducto.FirstOrDefault(pre => pre.GrupoPrecio == grupoprecio && pre.IdProducto == preEsp.IdProducto).Precio,
+                                                  }).ToList(),
+                                              }).ToList()
+                                   }).ToList()
+                               }
+                               ).ToList()
+
+                             }).ToListAsync();
+
+                    return Ok(colecciones[0].Edades);
+
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
         [Route("~/api/colecciones/listaprecios")]
         public async Task<IHttpActionResult> ObtenerColecciones([FromUri] ListaPrecio grupo)
         {
