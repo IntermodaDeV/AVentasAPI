07dd5ec4882c6da064d94a399f2e86802abcac7f
Francisco A. Garcia <frangarcia38@hotmail.com>
2021-04-12 07:48:09 -0600

    -Creacion de enpoint para tracking de geoposicion app.

--- AventasApi/Controllers/GeoposicionController.cs
+++ AventasApi/Controllers/GeoposicionController.cs
@@ -58,6 +58,34 @@ namespace AventasApi.Controllers
         }
 
         [HttpPost]
+        [Route("~/api/geoposicion/tracking")]
+        public IHttpActionResult PostTracking([FromBody] GoeposicionXAsesorViewModel geoposicion)
+        {
+            try
+            {
+                var user = _authenticationAppService.Validate(Request.Headers.Authorization.Parameter);
+
+                context.BitacoraGeoposicion.Add(new BitacoraGeoposicion
+                {
+                    IdAsignacionxAsesor = geoposicion.IdAsignacionxAsesor,
+                    Mocked = geoposicion.Mocked,
+                    Accuracy = geoposicion.Accuracy,
+                    Altitude = geoposicion.Altitude,
+                    Latitude = geoposicion.Latitude,
+                    Longitude = geoposicion.Longitude,
+                    CodigoAsesor = user.UserAccount,
+                    Fecha = DateTime.Now
+                });
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPost]
         [Route("~/api/Geoposicion/EnviarUbicacion")]
         public IHttpActionResult PostServicio([FromBody] GoeposicionXAsesorViewModel geoposicion)
         {
