0b285b6db232f5cc81175e13999c8e2e7c8948ae
Francisco A. Garcia <frangarcia38@hotmail.com>
2021-05-14 09:28:39 -0600

    Se añade metodo para crear proforma en bd.

--- AventasApi/Services/AsyncJobs/AsyncSqlInsert.cs
+++ AventasApi/Services/AsyncJobs/AsyncSqlInsert.cs
@@ -162,6 +162,52 @@ namespace AventasApi.Services.AsyncJobs
             }
         }
 
+        public static bool IngresarRecibosProforma(List<RecibosxClienteViewModel> recibos)
+        {
+            try
+            {
+
+                var reciboAAgregar = recibos.Select(rec => new RecibosProforma
+                {
+                    NumeroProforma = rec.NumeroRecibo,
+                    CodigoCliente = rec.CodigoCliente,
+                    Fecha = rec.Fecha,
+                    IdTipoPago = rec.IdTipoPago,
+                    Referencia = rec.Referencia,
+                    FechaCheque = rec.FechaPago,
+                    IdBanco = rec.IdBanco,
+                    Valor = rec.Valor,
+                    IdMoneda = rec.IdMoneda,
+                    CodigoAsesor = rec.CodigoAsesor,
+                    IdFactura = rec.IdFactura,
+                    Descuento = rec.Descuento,
+                    SpecPago = rec.SpecPago,
+                    UsuarioCreacion = rec.UsuarioCreacion,
+                    FechaCreacion = rec.FechaCreacion,
+                    RecibosProformaDetalle = rec.DetalleRecibo.Select(recDet => new RecibosProformaDetalle
+                    {
+                        IdProformaDetalle = recDet.IdReciboDetalle,
+                        ProformaId = recDet.ReciboId,
+                        IdSubFactura = recDet.IdSubFactura,
+                        Valor = recDet.Valor,
+                        Descuento = recDet.Descuento,
+                        EsAbono = recDet.EsAbono.Value,
+                    }).ToList()
+                }).ToList();
+
+                using (AVentasEntities context = new AVentasEntities())
+                {
+                    context.RecibosProforma.AddRange(reciboAAgregar);
+                    context.SaveChanges();
+                    return false;
+                }
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
         public static void IngresarRecibosFlotante(List<RecibosxClienteFlotanteViewModel> recibos)
         {
             var reciboTask = Task.Run(() =>
