2845e7305c06ff8dd748798ededf8c967f4b1b5a
Francisco A. Garcia <frangarcia38@hotmail.com>
2022-09-19 09:02:13 -0600

    Geocerca

---
+++ AventasApi/Models/ViewModels/ReportePedidoViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AventasApi.Models.ViewModels
+{
+    public class ReportePedidoViewModel
+    {
+        public string NumeroPedido { get; set; }
+        public string codigoCliente { get; set; }
+        public string nombre { get; set; }
+        public string asesor { get; set; }
+        public decimal? latitud { get; set; }
+        public decimal? longitud { get; set; }
+        public decimal? clienteLatitud { get; set; }
+        public decimal? clienteLongitud { get; set; }
+        public string distancia { get; set; }
+        public string estado { get; set; }
+    }
+}
