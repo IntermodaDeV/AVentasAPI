05c75ec2c6c8d5d91e22d6a928e28c5c638aa9fb
Carlos Bueso <cbueso@intermoda.com.hn>
2023-06-15 13:44:42 -0600

    asignacion linea usuario

--- AventasApi/Controllers/MaestroLineaController.cs
+++ AventasApi/Controllers/MaestroLineaController.cs
@@ -6,16 +6,20 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using AventasApi.Models.ViewModels;
 using System.Data.Entity;
+using AventasApi.Models.Authentication;
+using AventasApi.Services.Authentication;
 
 namespace AventasApi.Controllers
 {
     public class MaestroLineaController : ApiController
     {
         AVentasEntities context = new AVentasEntities();
+        private readonly AuthenticationAppService _authenticationAppService;
 
         public MaestroLineaController()
         {
             this.context.Database.CommandTimeout = 300;
+            _authenticationAppService = new AuthenticationAppService();
         }
 
         [HttpGet]
@@ -23,14 +27,19 @@ namespace AventasApi.Controllers
         {
             try
             {
-                List<LineaViewModel> lineas = await context.MaestroLinea.Where(ml => ml.Visible.Value).Select(ml => new LineaViewModel
+                var user = _authenticationAppService.Validate(Request.Headers.Authorization.Parameter);
+
+                var LineasAsignadas = await context.UsuarioLinea.Where(x => x.UsuarioId == user.Id && x.Asignada == true).Select(e => e.IdLinea).ToListAsync();
+
+                List<LineaViewModel> lineas = await context.MaestroLinea.Where(ml => ml.Visible.Value && LineasAsignadas.Contains(ml.IdLinea)).Select(ml => new LineaViewModel
                 {
                     IdLinea = ml.IdLinea,
                     Linea = ml.Linea,
                     Imagen = ml.Url_Imagen
                 }).OrderByDescending(x => x.IdLinea).ToListAsync();
                 return Ok(lineas);
-            }catch(Exception e)
+            }
+            catch (Exception e)
             {
                 return BadRequest(e.ToString());
             }
