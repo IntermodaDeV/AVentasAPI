9171e51211da83d4ae8981d7d2b88d60028a9fc9
Francisco A. Garcia <frangarcia38@hotmail.com>
2023-10-23 15:52:17 -0600

    - Envio dias de transporte en endpoint de cliente individual.

--- AventasApi/Controllers/ClienteController.cs
+++ AventasApi/Controllers/ClienteController.cs
@@ -1067,6 +1067,7 @@ namespace AventasApi.Controllers
                         GrupoPrecio = cli.GrupoPrecio,
                         GrupoCliente = cli.GrupoCliente,
                         Descuento = cli.Descuento,
+                        DiasTransporte = cli.DiasTransporte,
                         MaestroDescuento = ctx.Descuento.Where(x => x.Codigo.ToUpper() == cli.Descuento.ToUpper() && x.EmpresaId.ToUpper() == cli.EmpresaId.ToUpper() && x.activo == true).Select(x => new DescuentoViewModel
                         {
                             Codigo = x.Codigo,
