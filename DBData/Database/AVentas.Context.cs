c7c631a7450871ac00de57f6014beea709276569
lcaceres0022 <lucy_alcantara2000@hotmail.com>
2024-10-08 20:41:38 -0600

    [fix]: Correciones del modelo del merge de correo automatico y excel inventario branch.

--- DBData/Database/AVentas.Context.cs
+++ DBData/Database/AVentas.Context.cs
@@ -20,8 +20,9 @@ namespace DBData.Database
         public AVentasEntities()
             : base("name=AVentasEntities")
         {
+            var objectContext = (this as IObjectContextAdapter).ObjectContext; objectContext.CommandTimeout = 1440;
         }
-    
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
@@ -184,6 +185,7 @@ namespace DBData.Database
         public virtual DbSet<MailReceptors> MailReceptors { get; set; }
         public virtual DbSet<MailServicios> MailServicios { get; set; }
         public virtual DbSet<MailSpConfig> MailSpConfig { get; set; }
+        public virtual DbSet<ErroresLog> ErroresLog { get; set; }
     
         public virtual ObjectResult<CuentaCorriente_Result> CuentaCorriente(string codigoCliente)
         {
@@ -737,6 +739,36 @@ namespace DBData.Database
                 new ObjectParameter("CLIENTE", typeof(string));
     
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("SPDesactivarDireccionesCliente", cLIENTEParameter);
+        }
+    
+        public virtual int SP_EliminarInvenario(string numInventario)
+        {
+            var numInventarioParameter = numInventario != null ?
+                new ObjectParameter("numInventario", numInventario) :
+                new ObjectParameter("numInventario", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("SP_EliminarInvenario", numInventarioParameter);
+        }
+    
+        public virtual ObjectResult<SP_ObtenerProducto_Result> SP_ObtenerProducto(string producto, string talla, string color, string empresa)
+        {
+            var productoParameter = producto != null ?
+                new ObjectParameter("producto", producto) :
+                new ObjectParameter("producto", typeof(string));
+    
+            var tallaParameter = talla != null ?
+                new ObjectParameter("talla", talla) :
+                new ObjectParameter("talla", typeof(string));
+    
+            var colorParameter = color != null ?
+                new ObjectParameter("color", color) :
+                new ObjectParameter("color", typeof(string));
+    
+            var empresaParameter = empresa != null ?
+                new ObjectParameter("empresa", empresa) :
+                new ObjectParameter("empresa", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<SP_ObtenerProducto_Result>("SP_ObtenerProducto", productoParameter, tallaParameter, colorParameter, empresaParameter);
         }
     }
 }
