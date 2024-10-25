f162cd527ec3980e4d32d0a57cd5b5a9cdeda134
lcaceres0022 <lucy_alcantara2000@hotmail.com>
2024-10-24 23:09:15 -0600

    [fix]: Union ModelDB branch Publicacion y Mail estado cuenta.

--- DBData/Database/AVentas.Context.cs
+++ DBData/Database/AVentas.Context.cs
@@ -22,7 +22,7 @@ namespace DBData.Database
         {
             var objectContext = (this as IObjectContextAdapter).ObjectContext; objectContext.CommandTimeout = 1440;
         }
-    
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
@@ -186,6 +186,7 @@ namespace DBData.Database
         public virtual DbSet<MailServicios> MailServicios { get; set; }
         public virtual DbSet<MailSpConfig> MailSpConfig { get; set; }
         public virtual DbSet<ErroresLog> ErroresLog { get; set; }
+        public virtual DbSet<Servicio> Servicio { get; set; }
     
         public virtual ObjectResult<CuentaCorriente_Result> CuentaCorriente(string codigoCliente)
         {
