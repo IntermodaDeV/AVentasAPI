a47f3c004049c3fd739c5cbe4086ae8430afca87
LindaDiscua <ldiscua@intermoda.com.hn>
2026-08-26 08:36:38 -0600

    cambios en modelo para publicar

--- DBData/Database/Usuarios.cs
+++ DBData/Database/Usuarios.cs
@@ -57,8 +57,6 @@ namespace DBData.Database
         public bool FlagBodegaEspecifico { get; set; }
         public Nullable<bool> CorreoDevolucion { get; set; }
         public string Correo { get; set; }
-        public Nullable<int> CorrelativoRecibos { get; set; }
-        public string InicialesNombre { get; set; }
         public byte[] firma { get; set; }
     
         public virtual Empresa Empresa { get; set; }
